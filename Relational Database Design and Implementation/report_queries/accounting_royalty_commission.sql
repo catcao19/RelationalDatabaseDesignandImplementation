17191f09ab5c55aafd3ff068f06c96a33788eaea
Catherine Cao <79734146+catcao19@users.noreply.github.com>
2021-12-23 18:33:05 -0500

    Delete Relational Database Design and Implementation directory

--- Relational Database Design and Implementation/report_queries/accounting_royalty_commission.sql
+++
@@ -1,5 +0,0 @@
-/*retrive the royalty commission information */
-
-SELECT LeasedOfficeID, OfficeAddress,totalcommission*0.1 AS Royalty_Commission 
-FROM  Weekly_Total_Commission, Current_Leased_Office
-WHERE LeasedOfficeID = OfficeID;
--- Relational Database Design and Implementation/report_queries/legal_expiring_leases.sql
+++
@@ -1,8 +0,0 @@
-/*retrive all current leased offices with approaching lease end date*/
-
-SELECT    DISTINCT L.LeaseID, L.LeasedOfficeID, LeaseEndDate
-FROM      Leased_Office AS L, Lease_Holder AS LH
-WHERE     LeaseEndDate >= CURRENT_DATE
-AND       LeaseEndDate <= CURRENT_DATE + 90
-AND       L.LeaseID = LH.LeaseID
-ORDER BY  LeaseEndDate, L.LeasedOfficeID;
